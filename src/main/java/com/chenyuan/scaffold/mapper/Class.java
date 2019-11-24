1374457566ca3487734ce99630bd24d7513e4c6d
chenyuan <chenyuan190622@163.com>
2019-11-24 16:33:09 +0800

    添加class

---
+++ src/main/java/com/chenyuan/scaffold/mapper/Class.java
@@ -0,0 +1,14 @@
+package com.chenyuan.scaffold.mapper;
+
+/**
+ * @ClassName Class
+ * @Author chenyuan
+ * @Description TODO
+ * @Date 2019/11/24 16:30
+ */
+public class Class {
+
+    private String saa;
+    private Integer integer;
+    private String name;
+}
